27845cb34b0f6858b2def58ad96f156bd946b4e7
premsingh5 <48676860+premsingh5@users.noreply.github.com>
2021-03-18 13:02:32 +0530

    added file

---
+++ random_graph/graph.cpp
@@ -0,0 +1,41 @@
+#include "graph.hpp"
+
+// Constructor
+// TODO: copy constructor?
+Graph::Graph(int _n) : n(_n) {
+  parents.resize(n);
+  sizes.resize(n);
+  clear_edges();
+}
+
+void Graph::clear_edges() {
+  for (int i = 0; i < n; ++i) {
+    parents[i] = i;
+    sizes[i] = 1;
+  }
+  max_size = 1;
+}
+
+// Adds an edge using union by size
+// Does nothing if the edge already exists
+
+void Graph::add_edge(int a, int b) {
+  while (parents[a] != a) {
+    a = parents[a];
+  }
+  while (parents[b] != b) {
+    b = parents[b];
+  }
+  if (a != b) {
+    if (sizes[a] >= sizes[b]) {
+      parents[b] = a;
+      sizes[a] += sizes[b];
+      max_size = std::max(max_size, sizes[a]);
+    }
+    else {
+      parents[a] = b;
+      sizes[b] += sizes[a];
+      max_size = std::max(max_size, sizes[b]);
+    }
+  }
+}
